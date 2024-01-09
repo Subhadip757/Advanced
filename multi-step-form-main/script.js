4bcb875fc2f0943ea0aa57db29df435217314d05
Subhadip757 <SubhadipGitHub@gmail.com>
2024-01-09 16:03:54 +0530

    new files

---
+++ multi-step-form-main/script.js
@@ -0,0 +1,6 @@
+const step1 = document.querySelector('.step-1')
+const step2 = document.querySelector('.step-2')
+const step3 = document.querySelector('.step-3')
+const step4 = document.querySelector('.step-4')
+const step5 = document.querySelector('.step-5')
+
